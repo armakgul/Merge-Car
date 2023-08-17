d4f383e50da3ad736a563e6c45058f2b8a292448
cerkmen <cagrikaganerkmen@gmail.com>
2023-08-17 23:29:06 +0300

    IAP Testing Added
    IAP Testing Added

---
+++ Assets/Assets/Scripts/UI/StoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour
+{
+    public void On1CreditPurchaseCompeleted()
+    {
+        //activate 1000 credit method here
+    }
+
+    public void OnCar1PurchaseCompeleted()
+    {
+        //release of first forsale car method here
+    }
+
+    public void On5xPurchaseCompeleted()
+    {
+        //start 1 week 5x subscription method here
+    }
+}
