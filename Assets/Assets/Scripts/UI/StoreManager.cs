9052b6997b491b980f9aa60f2799758ee0bd1561
cerkmen <cagrikaganerkmen@gmail.com>
2023-08-21 22:41:15 +0300

    IAP STORE MANAGER
    Store manager güncellendi

--- Assets/Assets/Scripts/UI/StoreManager.cs
+++ Assets/Assets/Scripts/UI/StoreManager.cs
@@ -4,18 +4,75 @@ using UnityEngine;
 
 public class StoreManager : MonoBehaviour
 {
-    public void On1CreditPurchaseCompeleted()
+    //Credit Purchase methods Below (total 7 Credit Button)
+    public void OnCredit1PurchaseCompeleted()
     {
         //activate 1000 credit method here
     }
-
-    public void OnCar1PurchaseCompeleted()
+    public void OnCredit2PurchaseCompeleted()
     {
-        //release of first forsale car method here
+        //activate 5000 credit method here
+    }
+    public void OnCredit3PurchaseCompeleted()
+    {
+        //activate 10000 credit method here
+    }
+    public void OnCredit4PurchaseCompeleted()
+    {
+        //activate 20000 credit method here
+    }
+    public void OnCredit5PurchaseCompeleted()
+    {
+        //activate 40000 credit method here
+    }
+    public void OnCredit6PurchaseCompeleted()
+    {
+        //activate 80000 credit method here
+    }
+    public void OnCredit7PurchaseCompeleted()
+    {
+        //activate 160000 credit method here
     }
 
+    //Car Purchase Methods Here (total 5 car can be purchased)
+    public void OnCar1PurchaseCompeleted()
+    {
+        //release of 1. forsale car method here
+    }
+    public void OnCar2PurchaseCompeleted()
+    {
+        //release of 2. forsale car method here
+    }
+    public void OnCar3PurchaseCompeleted()
+    {
+        //release of 3. forsale car method here
+    }
+    public void OnCar4PurchaseCompeleted()
+    {
+        //release of 4. forsale car method here
+    }
+    public void OnCar5PurchaseCompeleted()
+    {
+        //release of 5. forsale car method here
+    }
+
+    //Subscription Purchase Methods Here (total 3 subscription)
+    public void On2xPurchaseCompeleted()
+    {
+        //start 1 week 2x subscription method here
+    }
+    public void On3xPurchaseCompeleted()
+    {
+        //start 1 week 3x subscription method here
+    }
     public void On5xPurchaseCompeleted()
     {
         //start 1 week 5x subscription method here
+    }
+
+    //Remove ADS purchase Method Here
+    public void OnRemoveADSPurchaseCompeleted()
+    {
+        //Remove ADS method here
     }
 }
