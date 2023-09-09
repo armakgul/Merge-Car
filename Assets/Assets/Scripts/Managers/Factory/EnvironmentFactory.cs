685f238bade43bdcdef163030fc3c19bf4f9b631
armakgul <armakgul@gmail.com>
2023-09-09 15:27:30 +0300

    09.09.2023 15.27
    * 20 adet bina düzgün yerleşecek şekilde eklendi

--- Assets/Assets/Scripts/Managers/Factory/EnvironmentFactory.cs
+++ Assets/Assets/Scripts/Managers/Factory/EnvironmentFactory.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class EnvironmentFactory : MonoBehaviour
 {
     public GameObject[] prefabs;
+
+    public GameObject CreateBuilding (GameObject building, Vector3 position, Quaternion rotation) {
+
+        GameObject buildingToSpawn = Instantiate(building, position, rotation);
+
+        return buildingToSpawn;
+
+    }
 }
